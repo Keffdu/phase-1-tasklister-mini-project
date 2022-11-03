31702c0b2922a0552991be5b0153381129db4b21
Kevin Dutzy <kddutz15@gmail.com>
2022-11-03 01:44:54 -0400

    final submission of tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+  const userInput = document.querySelector("#new-task-description");
+  const ul = document.querySelector("#tasks")
+  
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const li = document.createElement("li");
+  const deleteBtn = document.createElement("button");
+  li.textContent = userInput.value;
+  deleteBtn.textContent = "x";
+  ul.appendChild(li);
+  li.appendChild(   deleteBtn);
+  userInput.value = ""
+  deleteBtn.addEventListener("click", (e) => {
+    li.remove()
+  })
+})
+
+
+
 });
+
+
